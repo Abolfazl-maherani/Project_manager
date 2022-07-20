bfd56ccd310eefce83e3bb747566f01b9ed55b94
Abolfazl Maherani <mahrani.prj@gmail.com>
2022-07-21 01:50:32 +0330

    Fix #001: fix error validation and stop that when get first error.

--- app/http/validations/auth.js
+++ app/http/validations/auth.js
@@ -12,11 +12,14 @@ const USERNAME_REGEXP = {
 const registerValidator = () => [
   body("username")
     .notEmpty()
-    .withMessage("نام کاربری نباید خالی باشد")
+    .withMessage("نام کاربری وارد نشده است")
+    .bail()
     .isLength({ min: 4, max: 25 })
     .withMessage("نام کاربری باید بین 4 تا 25 کاراکتر باشد")
+    .bail()
     .matches(USERNAME_REGEXP.match)
     .withMessage(USERNAME_REGEXP.msg)
+    .bail()
     .custom(async (input) => {
       const user = await userModel.findOne({ username: input });
       if (user) throw "نام کاربری در سیستم موجود میباشد.";
@@ -25,27 +28,36 @@ const registerValidator = () => [
 
   body("email")
     .notEmpty()
-    .withMessage("ایمیل نباید خالی باشد")
+    .withMessage("ایمیل وارد نشده است")
+    .bail()
     .isEmail()
     .withMessage("ایمیل وارد شده صحیح نمیباشد")
+    .bail()
     .custom(async (input) => {
       const user = await userModel.findOne({ email: input });
       if (user) throw "ایمیل در سیستم موجود میباشد.";
       return true;
     }),
   body("mobile")
+    .notEmpty()
+    .withMessage("شماره موبایل وارد نشده است")
+    .bail()
     .isMobilePhone("fa-IR")
     .withMessage("شماره موبایل وارد شده صحیح نمیباشد")
+    .bail()
     .custom(async (input) => {
       const user = await userModel.findOne({ mobile: input });
       if (user) throw "شماره همراه در سیستم موجود میباشد.";
       return true;
     }),
   body("password")
+    .notEmpty()
+    .withMessage("رمز عبور وارد نشده است")
+    .bail()
     .isLength({ max: 16, min: 6 })
     .withMessage("رمز عبور حداقل 6 و حداکثر 16 کاراکتر باید باشد. ")
+    .bail()
     .custom((value, ctx) => {
-      if (!value) throw "رمز عبور وارد نشده است";
       if (value !== ctx.req?.body?.confirm_password)
         throw "رمز عبور با تکرار آن برابر نیست";
       return true;
@@ -56,6 +68,7 @@ const loginValidator = () => [
   body("username")
     .notEmpty()
     .withMessage("نام کاربری نمیتواند خالی باشد")
+    .bail()
     .matches(USERNAME_REGEXP.match)
     .withMessage(USERNAME_REGEXP.msg),
   body("password")
--- learnComment_Sample.js
+++ learnComment_Sample.js
@@ -37,6 +37,9 @@
 //+ Use variable in http-rest-client
 //# Can use {{}} to use variable in http-rest-client
 
+//+ bail method  in express-validator
+//# Stop error when get first error validation
+body("password").notEmpty().withMessage("رمز عبور وارد نشده است").bail();
 //? Wy save jwt token in db?
 //--End 100
 
